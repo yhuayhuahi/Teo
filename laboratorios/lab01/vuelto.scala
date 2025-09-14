fad3b247631c0fcb7d41ef3561c7eb106ed71924
Yourdyy Yossimar Huayhua Hillpa <yhuayhuahi@unsa.edu.pe>
2025-09-13 22:45:02 -0500

    Culminación del laboratorio 01

--- laboratorios/lab01/vuelto.scala
+++ laboratorios/lab01/vuelto.scala
@@ -1,20 +1,25 @@
-object Vuelto {
-  def countChange(money: Int, coins: List[Int]): Int = {
-    if (money == 0) 1                
-    else if (money < 0) 0            
-    else if (coins.isEmpty) 0        
-    else {
-      0 // valor temporal para que compile
-    }
-  }
-
-  def testFunction() {
-    println("Esta es una funcion")
-  }
-
-  def main(args: Array[string]) {
-    testFunction()
+def countChange(money: Int, coins: List[Int]): Int = {
+  if (money == 0) 1                // caso base hay 1 forma de dar cambio
+  else if (money < 0) 0            // si la cantidad es negativa
+  else if (coins.isEmpty) 0        // si no hay monedas disponibles no se puede dar cambio
+  else {
+    countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
+
+@main def runCountChange(): Unit = {
+  println("Ejemplo 1: " + countChange(4, List(1, 2)))    
+  // resultado esperado: 3
+
+  println("Ejemplo 2: " + countChange(10, List(2, 5, 3, 6)))
+  // resultado esperado: 5
+
+  println("Ejemplo 3: " + countChange(0, List(1, 2, 5)))
+  // resultado esperado:  1
+
+  println("Ejemplo 4: " + countChange(7, List(2, 4)))
+  // resultado esperado:0
+}
+
 
 
